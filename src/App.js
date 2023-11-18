c243bfb531fd4d14358ffd00a342d1f7c4cb1389
Arun <arunattri09@gmail.com>
2023-11-18 20:45:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './Components/navbar';
+import MainSection from './Components/MainSection';
+import Post from "./Components/Posts";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <Navbar/>
     </div>
+    <div className='MainSection'>
+      <MainSection />
+    </div>
+    <div className="PostSection">
+      <Post/>
+    </div>
+    </>
   );
 }
 
